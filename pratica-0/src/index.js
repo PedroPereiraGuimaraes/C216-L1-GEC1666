9cb87cbd1c23ed4cffb20f64bc735f5c7e7561b9
PedroPereiraGuimaraes <pedro.pereira@gec.inatel.br>
2024-03-08 22:56:52 -0300

    added pratica-0 exercices

---
+++ pratica-0/src/index.js
@@ -0,0 +1,9 @@
+// Exemplo de uso prático
+let nome = "Pedro Pereira Guimarães - 1666";
+function saudacaoPersonalizada(nome) {
+    console.log("Olá, " + nome + "!");
+}
+saudacaoPersonalizada(nome);
+
+let modulo = require('./module.js');
+modulo.saudacao();
---
+++ pratica-0/src/module.js
@@ -0,0 +1,6 @@
+module.exports = {
+    mensagem: 'Testando modulo: Pedro Pereira Guimarães - 1666!',
+    saudacao: function() {
+      console.log(this.mensagem);
+    }
+  };
